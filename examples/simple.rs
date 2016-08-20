81b767467f5aa0aadd56a81c20c5a9eb2feeffa0
Christian Iversen <ci@iversenit.dk>
2016-08-21 01:36:11 +0200

    * Improved example binary

--- examples/simple.rs
+++ examples/simple.rs
@@ -7,5 +7,8 @@ fn main()
     drop(colog::init());
     error!("error message");
     error!("error with fmt: {}", 42);
+    warn!("warn message");
     info!("info message");
+    debug!("debug message");
+    trace!("trace message");
 }
