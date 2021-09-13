4668abb0f2bef377406bf55f3966cb0793be5dd3
Christian Iversen <ci@iversenit.dk>
2021-09-13 14:52:51 +0200

    - Remove unnecessary &-annotation in match block (fixes clippy lint)

--- src/color.rs
+++ src/color.rs
@@ -6,10 +6,10 @@ pub fn level_color(level: &log::Level, msg: &str) -> String
 {
     match level
     {
-        &Level::Error => msg.red(),
-        &Level::Warn  => msg.yellow(),
-        &Level::Info  => msg.green(),
-        &Level::Debug => msg.green(),
-        &Level::Trace => msg.magenta(),
+        Level::Error => msg.red(),
+        Level::Warn  => msg.yellow(),
+        Level::Info  => msg.green(),
+        Level::Debug => msg.green(),
+        Level::Trace => msg.magenta(),
     }.bold().to_string()
 }
