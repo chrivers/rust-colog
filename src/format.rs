9b05e77866f2e20bf930c973f2ce361e39d3b156
Christian Iversen <ci@iversenit.dk>
2024-03-20 23:25:14 +0100

    + Documented everything relating to CologStyle trait

--- src/format.rs
+++ src/format.rs
@@ -6,14 +6,15 @@ use colored::*;
 use env_logger::fmt::Formatter;
 use log::{Level, Record};
 
-/// A style for [`colog`](crate)
+/// Customizable styles for [`colog`](crate)
 ///
 /// All functions on this trait come with a provided default implementation.
 /// This is how to the default style is provided.
 ///
-/// To create a custom style, make a new type, and implement [`CologStyle`] on
-/// it, overriding one or more of the default implementations on the trait.
+/// To create a custom style, make a new type which implements [`CologStyle`],
+/// overriding one or more of the default implementations on the trait.
 ///
+/// # Examples
 
 /// ```rust
 /// #[macro_use]
@@ -59,8 +60,30 @@ use log::{Level, Record};
 /// }
 /// ```
 
-
 pub trait CologStyle {
+    /// Format a message for a particular log level
+    ///
+    /// # Parameters
+    ///
+    /// - `level`: The log level of the message
+    /// - `msg`: The message text
+    ///
+    /// # Returns
+    ///
+    /// A [`String`] which is formatted according to the level. Typically, this
+    /// is done by wrapping the string in terminal color codes, appropriate for
+    /// the log level.
+    ///
+    /// # Defaults
+    ///
+    /// | Level            | Color   |
+    /// |------------------|---------|
+    /// | [`Level::Error`] | red     |
+    /// | [`Level::Warn`]  | yellow  |
+    /// | [`Level::Info`]  | green   |
+    /// | [`Level::Debug`] | green   |
+    /// | [`Level::Trace`] | magenta |
+
     fn level_color(&self, level: &log::Level, msg: &str) -> String {
         match level {
             Level::Error => msg.red(),
@@ -73,6 +96,29 @@ pub trait CologStyle {
         .to_string()
     }
 
+    /// Provide a "token" for a particular log level
+    ///
+    /// # Parameters
+    ///
+    /// - `level`: The log level of the message
+    ///
+    /// # Returns
+    ///
+    /// A [`&str`] which represents the token name for the given log level.
+    ///
+    /// This is typically a single letter (`"W"` for warn, `"D"` for debug, etc), or a short
+    /// string (`"WARN"`, `"DEBUG"`, etc)
+    ///
+    /// # Defaults
+    ///
+    /// | Level            | Token |
+    /// |------------------|-------|
+    /// | [`Level::Error`] | `"E"` |
+    /// | [`Level::Warn`]  | `"W"` |
+    /// | [`Level::Info`]  | `"*"` |
+    /// | [`Level::Debug`] | `"D"` |
+    /// | [`Level::Trace`] | `"T"` |
+
     fn level_token(&self, level: &Level) -> &str {
         match *level {
             Level::Error => "E",
@@ -83,6 +129,17 @@ pub trait CologStyle {
         }
     }
 
+    /// Construct the line prefix for a message of the given log level.
+    ///
+    /// This method is not typically overriden (rather [`Self::level_color`] or
+    /// [`Self::level_token`] is specialized), but is available for
+    /// customization
+    ///
+    /// # Defaults
+    ///
+    /// Formats the level token ([`Self::level_token`]) using
+    /// [`Self::level_color`], wrapped in `[` and `]` formatted in bold blue.
+
     fn prefix_token(&self, level: &Level) -> String {
         format!(
             "{}{}{}",
@@ -91,6 +148,21 @@ pub trait CologStyle {
             "]".blue().bold()
         )
     }
+
+    /// Top-level formatting function for log messages.
+    ///
+    /// This method is not typically overriden (rather [`Self::level_color`] or
+    /// [`Self::level_token`] is specialized), but is available for
+    /// customization.
+    ///
+    /// Overriding this method entirely changes the behavior of [`colog`](crate).
+    ///
+    /// # Defaults
+    ///
+    /// Inject `" | "` prefix at newlines, and format result according to
+    /// [`Self::prefix_token`].
+    ///
+    /// (this is the default [`colog`](crate) style)
 
     fn format(&self, buf: &mut Formatter, record: &Record<'_>) -> Result<(), Error> {
         let sep = format!("\n{} ", " | ".white().bold());
@@ -102,6 +174,19 @@ pub trait CologStyle {
         )
     }
 }
+
+/// Default colog style. Can manually be enabled like so:
+/// ```rust
+/// use log::info;
+/// use colog::format::{CologStyle, DefaultCologStyle};
+///
+/// let mut builder = env_logger::Builder::new();
+///
+/// builder.format(|buf, rec| DefaultCologStyle.format(buf, rec));
+/// /* ... */
+/// builder.init();
+///
+/// info!("logging ready");
 
 pub struct DefaultCologStyle;
 
