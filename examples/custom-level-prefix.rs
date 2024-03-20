410dad97b51fae4090915fe6c5167865facbd933
Christian Iversen <ci@iversenit.dk>
2024-03-20 22:51:24 +0100

    + Added example for overriding token prefixing

---
+++ examples/custom-level-prefix.rs
@@ -0,0 +1,33 @@
+#[macro_use]
+extern crate log;
+
+use colog::format::CologStyle;
+use colored::Colorize;
+use env_logger::Builder;
+use log::{Level, LevelFilter};
+
+pub struct CustomPrefixToken;
+
+impl CologStyle for CustomPrefixToken {
+    fn prefix_token(&self, level: &Level) -> String {
+        format!(
+            "{}{}{}",
+            "| ".blue().bold(),
+            self.level_color(level, self.level_token(level)),
+            " -->".blue().bold()
+        )
+    }
+}
+
+fn main() {
+    let mut builder = Builder::new();
+    builder.format(|buf, rec| CustomPrefixToken.format(buf, rec));
+    builder.filter(None, LevelFilter::Trace);
+    builder.init();
+    error!("error message");
+    error!("error with fmt: {}", 42);
+    warn!("warn message");
+    info!("info message");
+    debug!("debug message");
+    trace!("trace message");
+}
